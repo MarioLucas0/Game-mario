d423e8de69accabcbf502bf0fdb5ac2cef5fc33a
Lucas <mariolucas.2000@outlook.com>
2022-10-15 04:41:11 -0300

    :bug: fix: clouds game-over

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 
 const jump = ()   => {
 
@@ -12,6 +13,7 @@ const jump = ()   => {
 const loop = setInterval(()  =>  {
 
   const pipePosition = pipe.offsetLeft;
+  const cloudsPosition = clouds.offsetLeft;
   const marioPosition = Number(window.getComputedStyle(mario).bottom.replace('px', ''));
 
   if(pipePosition <= 120 && marioPosition < 80 && pipePosition > 0) {
@@ -22,13 +24,18 @@ const loop = setInterval(()  =>  {
     mario.style.animation = 'none';
     mario.style.bottom = `${marioPosition}px`;
 
+    clouds.style.animation = 'none';
+    clouds.style.left = `${cloudsPosition}px`;
+
     mario.src = '/img/game-over.png';
     mario.style.width = '75px';
     mario.style.marginLeft = '50px';
     mario.style.zIndex = '1';
 
+    
 
-    clearInterval(loop)
+
+    clearInterval(loop);
     
   }
 },10)
